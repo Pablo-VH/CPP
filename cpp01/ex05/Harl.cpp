34b56c8777d0415cd99a8305e2ad0088778f0ede
Pablo Vicente Hernandez <pavicent@c1r4s5.42madrid.com>
2025-08-25 17:48:30 +0200

    ex05

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,53 @@
+#include "Harl.hpp"
+
+void	Harl::debug( void )
+{
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout << "[ WRNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain( std::string level )
+{
+	int	i;
+	void (Harl:: *functions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string options[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	i = 0;
+	while (i < 4)
+	{
+		if (options[i] == level)
+		{
+			(this->*functions[i])();
+			return ;
+		}
+		i++;
+	}
+
+}
+
+Harl::Harl( void )
+{
+	std::cout << "Harl constructor" << std::endl;
+}
+Harl::~Harl( void )
+{
+	std::cout << "Harl destructor" << std::endl;
+}
