a2b9bd8b0a49de10f69ad2dec9642d849bb57ad5
Pablo-VH <anima23@imac-de-pablo.home>
2025-09-25 15:28:22 +0200

    cpp04/ex03

---
+++ cpp04/ex03/AMateria.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pavicent <pavicent@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 15:05:04 by pavicent          #+#    #+#             */
+/*   Updated: 2025/09/25 15:05:06 by pavicent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP_
+#define AMATERIA_HPP_
+
+#include <iostream>
+
+class AMateria
+{
+	protected:
+		std::string	_type;
+	public:
+		AMateria(void);
+		AMateria(std::string const & type);
+		AMateria(const AMateria& other);
+		AMateria& operator=(const AMateria& other);
+		~AMateria(void);
+
+		std::string const & getType() const;
+
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+
+};
+
+#endif
