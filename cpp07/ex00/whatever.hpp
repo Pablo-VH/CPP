a91f8dc34c2f5b5c98b3802229e8fa6b219d9808
Pablo-VH <pavicent@student.42madrid.com>
2026-01-26 18:38:02 +0100

    cpp07/ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,30 @@
+#ifndef WHATEVER_HPP_
+#define WHATEVER_HPP_
+
+#include <iostream>
+
+template <typename T>
+void	swap(T &x, T &y)
+{
+	T	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template <typename T>
+T const	&min(T &x, T &y)
+{
+	if (x < y)
+		return (x);
+	return (y);
+}
+
+template <typename T>
+T const	&max(T &x, T &y)
+{
+	if (x > y)
+		return (x);
+	return (y);
+}
+
+#endif
