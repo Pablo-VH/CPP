35dac93e0789f78fde178c0193b192e8989ebeca
Pablo Vicente Hernandez <pavicent@c3r9s1.42madrid.com>
2026-05-04 12:14:39 +0200

    cpp08/ex02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -5,13 +5,50 @@ int main()
 {
     MutantStack<int> m;
 
+
+	const MutantStack<int> cm(m);
+
+	MutantStack<int>::const_iterator cit = cm.begin();
+	MutantStack<int>::const_iterator cite = cm.end();
+	
+	std::cout << "\nIterando const MutantStack:" << std::endl;
+	
+	while (cit != cite)
+	{
+	    std::cout << *cit << std::endl;
+	    ++cit;
+	}
+    // 🔧 Meter datos
     m.push(5);
     m.push(17);
+
+    std::cout << "Top: " << m.top() << std::endl;
+
+    m.pop();
+
+    std::cout << "Size: " << m.size() << std::endl;
+
     m.push(3);
+    m.push(5);
+    m.push(737);
     m.push(42);
 
-    for (MutantStack<int>::iterator it = m.begin(); it != m.end(); ++it)
+    // 🔥 Iterar (LO IMPORTANTE DEL EJERCICIO)
+    std::cout << "\nIterando MutantStack:" << std::endl;
+
+    MutantStack<int>::iterator it = m.begin();
+    MutantStack<int>::iterator ite = m.end();
+
+    while (it != ite)
+    {
         std::cout << *it << std::endl;
+        ++it;
+    }
+
+    // 🧠 Comprobación con stack normal
+    std::stack<int> s(m);
+
+    std::cout << "\nStack normal (top): " << s.top() << std::endl;
 
     return 0;
-}
+} 
